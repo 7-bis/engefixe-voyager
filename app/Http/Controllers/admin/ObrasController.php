afca1dc2bcd01c9e35e85e341a13c91b8a71eb79
JuniorMaia2394 <maiajr2394@gmail.com>
2021-10-30 19:41:50 -0300

    adição do numero de telefone e email

--- app/Http/Controllers/admin/ObrasController.php
+++ app/Http/Controllers/admin/ObrasController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Diario;
 use App\Http\Controllers\Controller;
+use App\SysObra;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ObrasController extends Controller
 {
-    public function show() {
-        return 'asldajsldk';
+    public function show($id) {
+
+        $sysObra = SysObra::with('diarios')->find($id);
+       
+    return $sysObra;
+        
     }
 }
